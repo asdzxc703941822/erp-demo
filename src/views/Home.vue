2a7ae9ddb7781224138c9c92c45551521738a151
asdzxc703941822 <703941822@qq.com>
2019-05-31 00:25:43 +0800

    'dyc'

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!-- <a-button type="primary">Button</a-button> -->
+    <!-- <Layout>
+      <Header>header</Header>
+      <Layout>
+        <Sider>left sidebar</Sider>
+        <Content>main content</Content>
+        <Sider>right sidebar</Sider>
+      </Layout>
+      <Footer>footer</Footer>
+    </Layout>-->
+    <a-layout>
+      <a-layout-header></a-layout-header>
+      <a-layout-sider :trigger="null"></a-layout-sider>
+      <a-layout-content>
+        <a-button type="primary">Button</a-button>
+      </a-layout-content>
+      <a-layout-footer></a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
